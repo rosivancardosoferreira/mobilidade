1129273250dbcfb5ddfea39d91edc48694209ca6
Rosivan Cardoso Ferreira <rosivancardoso767@gmail.com>
2022-01-09 22:18:36 -0400

    feat: added title and touch-icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -48,10 +48,12 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&family=Source+Sans+Pro:wght@200;300;400;600;700&display=swap"
             rel="stylesheet"
           />
+          <title>II Seminário de Mobilidade Urbana de Itacoatiara 2022</title>
           <meta
             name="description"
-            content="II Seminário de Mobilidade Urbana de Itacoatiara 2022"
+            content="O evento visa debater sobre um tema atual em todas as cidades, que é a mobilidade urbana. Essa parece ser uma alternativa para melhorar a qualidade de vida das pessoas, através da busca de soluções para os problemas de transporte e de políticas públicas"
           ></meta>
+          <link rel="apple-touch-icon" href="/static/images/LOGO.png" />
           <link rel="icon" href="/static/favicon.ico" />
         </Head>
         <body>
