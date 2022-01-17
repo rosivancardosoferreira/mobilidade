a37a0365b625c04b343d71e12f4fa12726c5cc96
Rosivan Cardoso Ferreira <rosivancardoso767@gmail.com>
2022-01-17 19:11:43 -0400

    test: form netlify

--- src/presentation/sections/home/Form/index.tsx
+++ src/presentation/sections/home/Form/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import { Teste } from "./styles";
 import { Form, Field } from "react-final-form";
 import querystring from "qs";
@@ -6,6 +6,7 @@ import querystring from "qs";
 interface Props {
   nome: string;
   email: string;
+  arquivo: string;
 }
 const validate = (values: Props) => {
   const errors: any = {};
@@ -15,11 +16,20 @@ const validate = (values: Props) => {
   if (!values.email) {
     errors.email = "Campo obrigatório";
   }
+  if (!values.arquivo) {
+    errors.arquivo = "Campo obrigatório";
+  }
   return errors;
 };
 
 export function FormTeste() {
   const [enviado, setEnviado] = useState(false);
+
+  const [infosFile, setInfosFile] = useState({
+    name: "",
+    size: ""
+  });
+  const [alertSizeFile, setAlertSizeFile] = useState(false);
 
   const formSubmit =
     (formName: any, options: any = {}) =>
@@ -33,30 +43,23 @@ export function FormTeste() {
       return fetch("/", { method: "POST", headers, body });
     };
 
-  const onSubmit = useCallback(async (values, form) => {
-    try {
-      await formSubmit("contact")(values).then(() => {
-        alert("ENVIADO COM SUCESSO!");
-        setEnviado(true);
-      });
-      setTimeout(() => {
-        Object.keys(values).forEach(key => {
-          form.resetFieldState(key);
-        });
-        form.reset();
-      });
-    } catch (err) {
-      console.log(err);
-      alert("FALHOU COM SUCESSO!");
-    }
-  }, []);
+  async function onSubmit(values: any, form: any) {
+    console.log("requisição");
+    await formSubmit("contact", { hasFile: true })(values);
+    console.log("passou da requisição");
+    setInfosFile({ name: "", size: "" });
+    form.reset();
+    Object.keys(values).forEach(key => {
+      form.resetFieldState(key);
+    });
+  }
 
   return (
     <Teste>
       <Form
         validate={validate}
         onSubmit={onSubmit}
-        render={({ handleSubmit }) => (
+        render={({ handleSubmit, form }) => (
           <form name="contact" data-netlify="true" onSubmit={handleSubmit}>
             <input type="hidden" name="form-name" value="contact" />
 
@@ -81,6 +84,64 @@ export function FormTeste() {
                 </div>
               )}
             </Field>
+
+            <Field type="file" name="arquivo">
+              {({ input, meta }) => (
+                <>
+                  <div
+                    className={`content-file ${
+                      infosFile.name.length > 0 && "exist-file"
+                    }`}
+                  >
+                    <div className="file">
+                      <input
+                        {...input}
+                        id="analisisFile"
+                        // onChange={(e) => {
+                        //   input.onChange(e)
+                        //   handleChangeFile(e, form)
+                        // }}
+                      />
+                      <label htmlFor="analisisFile">
+                        Anexar fatura de energia*
+                      </label>
+                      <p
+                        className={
+                          (meta.error && meta.touched) || alertSizeFile
+                            ? "required-empty"
+                            : "required-empty-hidden"
+                        }
+                      >
+                        {meta.error && !alertSizeFile
+                          ? meta.error
+                          : "Arquivos de no máximo 1mb"}
+                      </p>
+                    </div>
+                    <span>.doc, .docx ou .pdf. até 1mb</span>
+                  </div>
+                  <div
+                    className={`list-files ${
+                      infosFile.name.length === 0 && "no-files"
+                    }`}
+                  >
+                    <div className="file-infos">
+                      <p className="file-name">{infosFile.name}</p>
+                      <p className="file-size">{infosFile.size} kb</p>
+                    </div>
+                    <button
+                      onClick={() => {
+                        form.change("arquivo", undefined);
+                        console.log("limpar aquivo");
+                        setInfosFile({ name: "", size: "" });
+                      }}
+                    >
+                      TESTE
+                    </button>
+                  </div>
+                </>
+              )}
+            </Field>
+
             <Field name="email">
               {({ input, meta }) => (
                 <div className="message-newslatter">
